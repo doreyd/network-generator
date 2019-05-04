c1e4551c02e7f0b2e368417c2d1e49c86831334b
Doreyd Mehila <dmehila@gmail.com>
2019-05-04 21:47:02 +0200

    First working version of a basic random node generator

--- networkGen.js
+++ networkGen.js
@@ -1,3 +1,6 @@
+// Dom selection
+let svg = document.getElementById("svg");
+
 // Function to create and set up SVG elements
 const nodeGenerator = (type, props = {}) => {
   let newElem = document.createElementNS("http://www.w3.org/2000/svg", type);
@@ -5,3 +8,27 @@ const nodeGenerator = (type, props = {}) => {
   svg.append(newElem);
   return newElem;
 };
+
+// Function generating random numbers between 0 and the picked base
+const ran = base => Math.round(Math.random() * base);
+
+// Function to generate a random nosed list
+const nodeslistGen = qty => {
+  let nodes = [];
+  for (let i = 0; i < qty; i++) nodes.push([ran(800), ran(1300), ran(100)]);
+  return nodes;
+};
+
+// Function to generate the SVG nodes
+const nodesGen = nodesList => {
+  nodesList.forEach(x => {
+    nodeGenerator("circle", {
+      cy: x[0],
+      cx: x[1],
+      r: x[2],
+      fill: "steelblue"
+    });
+  });
+};
+
+nodesGen(nodeslistGen(30));
