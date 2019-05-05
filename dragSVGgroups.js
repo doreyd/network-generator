8ac00a3b1d379d21f7a7104077ac65db85b62c87
Doreyd Mehila <dmehila@gmail.com>
2019-05-05 02:04:33 +0200

    added dragSVGgroups module that allows for draging of multiple SVG objects as an attachement

--- dragSVGgroups.js
+++ dragSVGgroups.js
@@ -1,43 +1,53 @@
+const dragSVGgroup = elemGroupId => {
+  let elemGroup = elemGroupId.map(x => document.getElementById(x));
+  elemGroup.forEach(x => dragSVG(x, elemGroup));
+};
+
 // This is a module for drag and drop SVG elements
-const dragSVG = elem => {
-  let deltaX = 0;
-  let deltaY = 0;
+const dragSVG = (elem, elemGroup) => {
+  let delta = [];
   let elemDragged;
 
-  const coord = e => {
+  const coord = (e, elem) => {
     let cursX = e.pageX;
     let cursY = e.pageY;
-    let elemX = elemDragged.x
-      ? elemDragged.x.animVal.value
-      : elemDragged.cx.animVal.value;
-    let elemY = elemDragged.y
-      ? elemDragged.y.animVal.value
-      : elemDragged.cy.animVal.value;
+    let elemX = elem.x ? elem.x.animVal.value : elem.cx.animVal.value;
+    let elemY = elem.y ? elem.y.animVal.value : elem.cy.animVal.value;
     return [cursX, cursY, elemX, elemY];
   };
 
-  const newPos = e => {
-    let d2 = coord(e);
-    if (elemDragged.cx) {
-      elemDragged.setAttribute("cx", d2[0] - deltaX);
-      elemDragged.setAttribute("cy", d2[1] - deltaY);
-    } else if (elemDragged.x) {
-      elemDragged.setAttribute("x", d2[0] - deltaX);
-      elemDragged.setAttribute("y", d2[1] - deltaY);
+  const getDelta = (e, elem, i) => {
+    let d2 = coord(e, elem);
+    if (elem.cx) {
+      elem.setAttribute("cx", d2[0] - delta[i][0]);
+      elem.setAttribute("cy", d2[1] - delta[i][1]);
+    } else if (elem.x) {
+      elem.setAttribute("x", d2[0] - delta[i][0]);
+      elem.setAttribute("y", d2[1] - delta[i][1]);
     }
+  };
+
+  const newPos = e => {
+    elemGroup.forEach((x, i) => {
+      getDelta(e, x, i);
+    });
+  };
+
+  const setDelta = (e, elem) => {
+    let d = coord(e, elem);
+    delta.push([d[0] - d[2], d[1] - d[3]]);
   };
 
   elem.onmousedown = e => {
     elemDragged = e.target;
-    let d = coord(e);
-    deltaX = d[0] - d[2];
-    deltaY = d[1] - d[3];
+    elemGroup.forEach(x => {
+      setDelta(e, x);
+    });
     svg.onmousemove = e => newPos(e);
   };
 
   elem.onmouseup = e => {
-    deltaX = 0;
-    deltaY = 0;
+    delta = [];
     elemDragged = "";
     svg.onmousemove = e => {};
   };
